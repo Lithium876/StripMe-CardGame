4534f111ece792411409f297318e6911bad88924
Lomar Lilly <lithiumlilly3@gmail.com>
2017-11-24 18:58:28 -0500

    implement a 'showGreeting' display

---
+++ Assets/util.py
@@ -0,0 +1,18 @@
+from Assets.ADT import *
+from Assets.cards import *
+
+showGreeting = """\
+********************************************************
+* WELCOME to Strip Me 								   *
+* 													   *
+* Rules of the game: Check the docs 				   *
+* 													   *
+* Game Play: 										   *
+* - player 0: the computer 							   *
+* - player 1: you 									   *
+* - enter: play the top card and place on discard pile *
+* - q: quit i.e. stop playing the game 				   *
+* 													   *
+* Enjoy! 											   *
+********************************************************
+"""
