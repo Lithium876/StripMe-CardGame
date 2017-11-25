c68f7a894167fef861c0623bd378f2efe90b46f6
Lomar Lilly <lithiumlilly3@gmail.com>
2017-11-24 19:00:57 -0500

    implemented a dictionary to store pay cards
    Include a small dictionary called payCards. There must be 4 elements. Each element has the format face: pay rate. For example 'K':3 stores the information that a king's pay rate is 3.

--- Assets/util.py
+++ Assets/util.py
@@ -3,16 +3,17 @@ from Assets.cards import *
 
 showGreeting = """\
 ********************************************************
-* WELCOME to Strip Me 								   *
-* 													   *
-* Rules of the game: Check the docs 				   *
-* 													   *
-* Game Play: 										   *
-* - player 0: the computer 							   *
-* - player 1: you 									   *
+* WELCOME to Strip Me 								                 *
+* 													                           *
+* Rules of the game: Check the docs 				           *
+* 													                           *
+* Game Play: 										                       *
+* - player 0: the computer 							               *
+* - player 1: you 									                   *
 * - enter: play the top card and place on discard pile *
-* - q: quit i.e. stop playing the game 				   *
-* 													   *
-* Enjoy! 											   *
+* - q: quit i.e. stop playing the game 				         *
+* 													                           *
+* Enjoy! 											                         *
 ********************************************************
 """
+payCards = {'A': 4, 'K': 3, 'Q': 2, 'J': 1}
