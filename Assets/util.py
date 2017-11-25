c50d53c5dcf59c569d1bbf7341047055d9ee146f
Lomar Lilly <lithiumlilly3@gmail.com>
2017-11-24 19:02:12 -0500

    implement isPayCard function
    Implemented a function that determines whether a card is a payment card. Name the function isPayCard

--- Assets/util.py
+++ Assets/util.py
@@ -17,3 +17,13 @@ showGreeting = """\
 ********************************************************
 """
 payCards = {'A': 4, 'K': 3, 'Q': 2, 'J': 1}
+
+def isPayCard(card):
+	'''
+	Function that determines whether a card is a pay
+	card. Name the function isPayCard
+	'''
+	if card[1] in payCards.keys():
+		return True
+	else:
+		return False
